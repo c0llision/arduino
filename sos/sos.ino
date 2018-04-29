4cf0140a01a3b28c5d07373613626cba532fe388
c0llision <0xmw@protonmail.com>
2018-04-30 00:24:48 +0100

    do sos on setup only

--- sos/sos.ino
+++ sos/sos.ino
@@ -5,6 +5,7 @@ int DOT = 250;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  sos();
 }
 
 void flash(int t)
@@ -33,5 +34,5 @@ void sos()
 
 // the loop function runs over and over again forever
 void loop() {
-  sos();
+
 }
